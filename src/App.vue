1249471da2bb58024fd794ccaff979a905087d9e
devguchi <devguchitaro@gmail.com>
2021-09-20 18:21:18 +0900

    fix colors

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,27 @@
 <template>
 <div>
-<h1>
-  開発グチ太郎
-</h1>
-<ul>
-<li>
-  <a href="https://github.com/devguchi">
-    Github
-  </a>
-</li>
-<li>
-  <a href="https://twitter.com/devguchi">
-    Twitter
-  </a>
-</li>
-<li>
-  <a href="https://atcoder.jp/users/devguchi">
-    AtCoder
-  </a>
-</li>
-<li>
-  <a href="https://www.kaggle.com/devguchi">
-    Kaggle
-  </a>
-</li>
-</ul>
+  <Home/>
 </div>
 </template>
 
 <script>
+import Home from '@/components/Home'
 export default {
   name: 'App',
+  components: {Home},
 }
 </script>
 
+<style lang="scss">
+body {
+  background: black;
+  color: white;
+  padding:0 10px;
+}
+a {
+  color:#0f0;
+}
+a:visited {
+  color: #0c0;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+<h1>
+  開発グチ太郎
+</h1>
+<ul>
+<li>
+  <a href="https://github.com/devguchi">
+    Github
+  </a>
+</li>
+<li>
+  <a href="https://twitter.com/devguchi">
+    Twitter
+  </a>
+</li>
+<li>
+  <a href="https://atcoder.jp/users/devguchi">
+    AtCoder
+  </a>
+</li>
+<li>
+  <a href="https://www.kaggle.com/devguchi">
+    Kaggle
+  </a>
+</li>
+</ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+

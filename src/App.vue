6545b944d72856929e326f73bf788a3d29662471
devguchi <devguchitaro@gmail.com>
2021-10-30 01:13:38 +0900

    added prettier and formatted

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,10 @@ export default {
 body {
   background: black;
   color: white;
-  padding:0 10px;
+  padding: 0 10px;
 }
 a {
-  color:#0f0;
+  color: #0f0;
 }
 a:visited {
   color: #0c0;
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/pages/Home'
 import Portfolio from '@/pages/Portfolio'
 import NotFound from '@/pages/NotFound'
 
 const routes = [
-    { path: '/', component: Home },
-    { path: '/portfolio', component: Portfolio },
-    { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound },
-    { path: '/:pathMatch(.*)', name: 'bad-not-found', component: NotFound },
+  { path: '/', component: Home },
+  { path: '/portfolio', component: Portfolio },
+  { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound },
+  { path: '/:pathMatch(.*)', name: 'bad-not-found', component: NotFound },
 ]
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 })
 export default router

a7810f22826035cfbe06bc1507b927bb3d502719
devguchi <devguchitaro@gmail.com>
2021-10-30 01:39:31 +0900

    added twitter button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,25 @@
   <router-link to="/">
     <h1>開発グチ太郎</h1>
   </router-link>
+  <a
+    href="https://twitter.com/devguchi?ref_src=twsrc%5Etfw"
+    class="twitter-follow-button"
+    data-show-count="false"
+  >
+    Follow @devguchi
+  </a>
 </template>
+
+<script>
+export default {
+  name: 'header',
+  mounted() {
+    let tweetScript = document.createElement('script')
+    tweetScript.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+    document.head.appendChild(tweetScript)
+  },
+}
+</script>
 
 <style scoped>
 h1 {

0dfb87518d400fb4e05fa59866d6481004eba60b
devguchi <devguchitaro@gmail.com>
2021-11-05 18:10:14 +0900

    code format

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -2,13 +2,19 @@
   <Default>
     <h2>Portfolio</h2>
     <ul>
-      <li><a href="https://github.com/devguchi/calc-app" target="_blank">calc-app</a></li>
+      <li>
+        <a href="https://github.com/devguchi/calc-app" target="_blank">
+          calc-app
+        </a>
+      </li>
       <ul>
         <li>
           Vue2+Vuetify+TypeScript+firebase hostingで計算アプリを作成しました
         </li>
         <li>
-          <a href="https://calc-777c0.web.app/" target="_blank">sample site</a>
+          <a href="https://calc-777c0.web.app/" target="_blank">
+            sample site
+          </a>
         </li>
       </ul>
     </ul>
@@ -17,6 +23,7 @@
 
 <script>
 import Default from '@/layouts/Default'
+
 export default {
   name: 'Portfolio',
   components: { Default },

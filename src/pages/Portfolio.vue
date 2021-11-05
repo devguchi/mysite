397b78d87ceab4c264d8f906a7ac178ecc2e2817
devguchi <devguchitaro@gmail.com>
2021-11-05 18:02:50 +0900

    added calc-app to portfolio

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,5 +1,18 @@
 <template>
-  <Default> Portfolio </Default>
+  <Default>
+    <h2>Portfolio</h2>
+    <ul>
+      <li><a href="https://github.com/devguchi/calc-app" target="_blank">calc-app</a></li>
+      <ul>
+        <li>
+          Vue2+Vuetify+TypeScript+firebase hostingで計算アプリを作成しました
+        </li>
+        <li>
+          <a href="https://calc-777c0.web.app/" target="_blank">sample site</a>
+        </li>
+      </ul>
+    </ul>
+  </Default>
 </template>
 
 <script>
